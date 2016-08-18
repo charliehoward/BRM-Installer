ffdd84aee57e9bb420628990e230881c80006590
Charlie Howard <charlie@howardgb.com>
2016-08-18 16:22:28 +0100

    Delete cleanup.bat

--- cleanup.bat
+++
@@ -1,9 +0,0 @@
-@ECHO off
-DEL "C:\Windows\BRM Computers\main.bat"
-DEL "C:\Windows\BRM Computers\kisbrm.nupkg"
-DEL "C:\Windows\BRM Computers\SetTaskbar.vbs"
-DEL "C:\Windows\BRM Computers\assets.bat"
-DEL "C:\Windows\BRM Computers\PinTo10.exe"
-@ECHO BRM Installer has completed, press any key to exit.
->NUL PAUSE
-EXIT
