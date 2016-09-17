761a7d9e340c958095a0794740c00575d4ecfce2
Charlie Howard <charlie@howardgb.com>
2016-09-17 19:11:00 +0100

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,11 +1,46 @@
 @ECHO off
-@ECHO BRM Installer 3.1.0.0
->NUL TIMEOUT /t 10 /NOBREAK
 cURL -o "C:\Windows\BRM Computers\oem.bmp" "https://raw.githubusercontent.com/TheJumpyWizard/BRM-Installer/master/oem.bmp" --insecure
-cURL -o "C:\Windows\BRM Computers\kaspersky17.nupkg" "https://raw.githubusercontent.com/TheJumpyWizard/BRM-Installer/master/kaspersky17.nupkg" --insecure
+cURL -o "C:\Windows\BRM Computers\kaspersky.nupkg" "https://raw.githubusercontent.com/TheJumpyWizard/BRM-Installer/master/kaspersky.nupkg" --insecure
 cURL -o "C:\Windows\BRM Computers\SetTaskbar.vbs" "https://raw.githubusercontent.com/TheJumpyWizard/BRM-Installer/master/SetTaskbar.vbs" --insecure
 cURL -o "C:\Windows\BRM Computers\PinTo10.exe" "https://raw.githubusercontent.com/TheJumpyWizard/BRM-Installer/master/PinTo10.exe" --insecure
-:START
+@ECHO off
+@ECHO.
+@ECHO.
+@ECHO 					-------------------------------------------
+@ECHO 					           BRM Installer 3.1.1.0
+@ECHO 					-------------------------------------------
+@ECHO.
+@ECHO.
+@ECHO.
+@ECHO.
+@ECHO.
+@ECHO.
+@ECHO.
+@ECHO.
+@ECHO.
+@ECHO.
+@ECHO.
+@ECHO.
+@ECHO.
+@ECHO.
+@ECHO.
+@ECHO.
+@ECHO.
+@ECHO.
+@ECHO.
+:QUESTION
+@ECHO 1. Install all programs.
+@ECHO 2. Install all programs except for Kaspersky Internet Security.
+@ECHO 3. Install all programs except for LibreOffice.
+@ECHO 4. Install all programs except for Kaspersky Internet Security and LibreOffice.
+@ECHO.
+@ECHO.
+SET /P MODE=What version would you like to install: 
+IF "%MODE%" == "1" GOTO 1
+IF "%MODE%" == "2" GOTO 2
+IF "%MODE%" == "3" GOTO 3
+IF "%MODE%" == "3" GOTO 4
+:1
 @ECHO Installing BRM OEM ...
 REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v Logo /t REG_SZ /d "C:\Windows\BRM Computers\oem.bmp" /reg:64 /f
 REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v Manufacturer /t REG_SZ /d "BRM Computers" /reg:64 /f
@@ -13,27 +48,111 @@ REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v Support
 REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v SupportPhone /t REG_SZ /d "08712 244129" /reg:64 /f
 REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v SupportURL /t REG_SZ /d "http://www.brmcomputers.co.uk" /reg:64 /f
 @ECHO Installing Microsoft .NET 4.5
-CHOCO install dotnet4.5 --confirm --allow-empty-checksums
+CHOCO install dotnet4.5 --confirm
 @ECHO Installing Google Chrome ...
-CHOCO install googlechrome --confirm --allow-empty-checksums
+CHOCO install googlechrome --confirm
 @ECHO Installing Firefox ...
-CHOCO install firefox -packageParameters "l=en-GB" --confirm --allow-empty-checksums
-@ECHO Installing VLC Media Player ...
-CHOCO install vlc --confirm --allow-empty-checksums
-@ECHO Installing 7zip ...
-CHOCO install 7zip --confirm --allow-empty-checksums
-@ECHO Installing Adobe Flash Player ...
-CHOCO install flashplayerplugin --confirm --allow-empty-checksums
-@ECHO Installing Java ...
-CHOCO install javaruntime --confirm --allow-empty-checksums
-@ECHO Installing Adobe Reader ...
-CHOCO install adobereader --confirm --allow-empty-checksums
+CHOCO install firefox --confirm
 @ECHO Installing Kaspersky Internet Security 2017 ...
-CHOCO install "C:\Windows\BRM Computers\kaspersky17.nupkg" --confirm --allow-empty-checksums
+CHOCO install "C:\Windows\BRM Computers\kaspersky.nupkg" --confirm
+@ECHO Installing VLC Media Player ...
+CHOCO install vlc --confirm
+@ECHO Installing 7zip ...
+CHOCO install 7zip --confirm
+@ECHO Installing Adobe Flash Player ...
+CHOCO install flashplayerplugin --confirm
+@ECHO Installing Java ...
+CHOCO install javaruntime --confirm
+@ECHO Installing Adobe Reader ...
+CHOCO install adobereader --confirm
 @ECHO Installing LibreOffice ...
-CHOCO install libreoffice  --confirm --allow-empty-checksums
+CHOCO install libreoffice  --confirm
 @ECHO Installing TeamViewer ...
-CHOCO install teamviewer --confirm --allow-empty-checksums
+CHOCO install teamviewer --confirm
+GOTO END
+:2
+@ECHO Installing BRM OEM ...
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v Logo /t REG_SZ /d "C:\Windows\BRM Computers\oem.bmp" /reg:64 /f
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v Manufacturer /t REG_SZ /d "BRM Computers" /reg:64 /f
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v SupportHours /t REG_SZ /d "Mon-Fri 9am-5pm - Sat 9am-4pm" /reg:64 /f
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v SupportPhone /t REG_SZ /d "08712 244129" /reg:64 /f
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v SupportURL /t REG_SZ /d "http://www.brmcomputers.co.uk" /reg:64 /f
+@ECHO Installing Microsoft .NET 4.5
+CHOCO install dotnet4.5 --confirm
+@ECHO Installing Google Chrome ...
+CHOCO install googlechrome --confirm
+@ECHO Installing Firefox ...
+CHOCO install firefox --confirm
+@ECHO Installing VLC Media Player ...
+CHOCO install vlc --confirm
+@ECHO Installing 7zip ...
+CHOCO install 7zip --confirm
+@ECHO Installing Adobe Flash Player ...
+CHOCO install flashplayerplugin --confirm
+@ECHO Installing Java ...
+CHOCO install javaruntime --confirm
+@ECHO Installing Adobe Reader ...
+CHOCO install adobereader --confirm
+@ECHO Installing LibreOffice ...
+CHOCO install libreoffice  --confirm
+@ECHO Installing TeamViewer ...
+CHOCO install teamviewer --confirm
+GOTO END
+:3
+@ECHO Installing BRM OEM ...
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v Logo /t REG_SZ /d "C:\Windows\BRM Computers\oem.bmp" /reg:64 /f
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v Manufacturer /t REG_SZ /d "BRM Computers" /reg:64 /f
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v SupportHours /t REG_SZ /d "Mon-Fri 9am-5pm - Sat 9am-4pm" /reg:64 /f
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v SupportPhone /t REG_SZ /d "08712 244129" /reg:64 /f
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v SupportURL /t REG_SZ /d "http://www.brmcomputers.co.uk" /reg:64 /f
+@ECHO Installing Microsoft .NET 4.5
+CHOCO install dotnet4.5 --confirm
+@ECHO Installing Google Chrome ...
+CHOCO install googlechrome --confirm
+@ECHO Installing Firefox ...
+CHOCO install firefox --confirm
+@ECHO Installing Kaspersky Internet Security 2017 ...
+CHOCO install "C:\Windows\BRM Computers\kaspersky.nupkg" --confirm
+@ECHO Installing VLC Media Player ...
+CHOCO install vlc --confirm
+@ECHO Installing 7zip ...
+CHOCO install 7zip --confirm
+@ECHO Installing Adobe Flash Player ...
+CHOCO install flashplayerplugin --confirm
+@ECHO Installing Java ...
+CHOCO install javaruntime --confirm
+@ECHO Installing Adobe Reader ...
+CHOCO install adobereader --confirm
+@ECHO Installing TeamViewer ...
+CHOCO install teamviewer --confirm
+GOTO END
+:4
+@ECHO Installing BRM OEM ...
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v Logo /t REG_SZ /d "C:\Windows\BRM Computers\oem.bmp" /reg:64 /f
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v Manufacturer /t REG_SZ /d "BRM Computers" /reg:64 /f
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v SupportHours /t REG_SZ /d "Mon-Fri 9am-5pm - Sat 9am-4pm" /reg:64 /f
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v SupportPhone /t REG_SZ /d "08712 244129" /reg:64 /f
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation /v SupportURL /t REG_SZ /d "http://www.brmcomputers.co.uk" /reg:64 /f
+@ECHO Installing Microsoft .NET 4.5
+CHOCO install dotnet4.5 --confirm
+@ECHO Installing Google Chrome ...
+CHOCO install googlechrome --confirm
+@ECHO Installing Firefox ...
+CHOCO install firefox --confirm
+@ECHO Installing VLC Media Player ...
+CHOCO install vlc --confirm
+@ECHO Installing 7zip ...
+CHOCO install 7zip --confirm
+@ECHO Installing Adobe Flash Player ...
+CHOCO install flashplayerplugin --confirm
+@ECHO Installing Java ...
+CHOCO install javaruntime --confirm
+@ECHO Installing Adobe Reader ...
+CHOCO install adobereader --confirm
+@ECHO Installing TeamViewer ...
+CHOCO install teamviewer --confirm
+GOTO END
+:END
 VER | FINDSTR /i "6\.1\." > nul
 IF %ERRORLEVEL% EQU 0 GOTO WINDOWS7
 VER | FINDSTR /i "6\.2\." > nul
@@ -112,7 +231,7 @@ REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Taskband" /v "F
 GOTO CLEANUP
 :CLEANUP
 @ECHO Cleaning up ...
-DEL "C:\Windows\BRM Computers\kisbrm.nupkg"
+DEL "C:\Windows\BRM Computers\kaspersky.nupkg"
 DEL "C:\Windows\BRM Computers\SetTaskbar.vbs"
 DEL "C:\Windows\BRM Computers\PinTo10.exe"
 @ECHO BRM Installer has completed, press any key to exit.
