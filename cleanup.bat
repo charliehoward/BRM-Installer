3b94d079047f4fbdbec8ddf0a369d7613a33633e
Charlie Howard <charlie@howardgb.com>
2016-09-17 19:16:13 +0100

    Create cleanup.bat

---
+++ cleanup.bat
@@ -0,0 +1,8 @@
+@ECHO Cleaning up ...
+DEL "C:\Windows\BRM Computers\kaspersky.nupkg"
+DEL "C:\Windows\BRM Computers\SetTaskbar.vbs"
+DEL "C:\Windows\BRM Computers\PinTo10.exe"
+DEL "C:\Windows\BRM Computers\installer.bat"
+@ECHO BRM Installer has completed, press any key to exit.
+>NUL PAUSE
+EXIT
